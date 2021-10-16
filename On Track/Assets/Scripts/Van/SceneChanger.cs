e402b8d975dd89f563a4b8dd5c71ccbf7def833d
Van Phan <80922094+Van-Phan-001@users.noreply.github.com>
2021-10-16 06:57:33 -0400

    Main menu functionality
    Implemented images into main menu scene. Added a scene manager prefab. Starting to put assets into media folder.

--- On Track/Assets/Scripts/Van/SceneChanger.cs
+++ On Track/Assets/Scripts/Van/SceneChanger.cs
@@ -9,14 +9,17 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class SceneChanger : MonoBehaviour
 {
+    [Header("Testing values")]
     [SerializeField] private bool playTutorial;
 
     /// <summary>
-    /// 
+    /// Player prefs can be accessed via
     /// </summary>
     private void Start()
     {
-        
+        //0 represents if we have not been to scene
+        PlayerPrefs.SetFloat("playTutorial", 0);
+        //PlayerPrefs.GetFloat("playerTutorial"); //line of code to adjust player pref values 
     }
     public void ToMainMenu()
     {
@@ -24,6 +27,12 @@ public class SceneChanger : MonoBehaviour
     }
     public void ToGame()
     {
+        if (PlayerPrefs.GetFloat("playTutorial") < 1 || playTutorial)
+        {
+            PlayerPrefs.SetFloat("playTutorial", 1);
+            SceneManager.LoadScene("TutorialScene");
+            return;
+        }  
         SceneManager.LoadScene("Game");
     }
     public void ToTutorialScene()
