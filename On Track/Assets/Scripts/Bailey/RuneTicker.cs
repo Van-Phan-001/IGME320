c4205a772b74f057b662dcedc7116dfc17ea7708
ztl3132 <ztl3132@rit.edu>
2021-10-22 16:08:06 -0400

    Added changing runes

--- On Track/Assets/Scripts/Bailey/RuneTicker.cs
+++ On Track/Assets/Scripts/Bailey/RuneTicker.cs
@@ -12,6 +12,8 @@ public class RuneTicker : MonoBehaviour
     Button downTicker;
     [SerializeField]
     Text displayText;
+    [SerializeField]
+    Image displayImage;
 
     [SerializeField]
     int slotId;
@@ -20,6 +22,19 @@ public class RuneTicker : MonoBehaviour
     GameManager gm;
     [SerializeField]
     int currentValue;
+
+    [SerializeField]
+    Sprite rune1;
+    [SerializeField]
+    Sprite rune2;
+    [SerializeField]
+    Sprite rune3;
+    [SerializeField]
+    Sprite rune4;
+    [SerializeField]
+    Sprite rune5;
+    [SerializeField]
+    Sprite rune6;
 
     private void Start()
     {
@@ -32,21 +47,27 @@ public class RuneTicker : MonoBehaviour
         {
             case 1: 
                 displayText.text = "I";
+                displayImage.sprite = rune1;
                 break;
             case 2:
                 displayText.text = "II";
+                displayImage.sprite = rune2;
                 break;
             case 3:
                 displayText.text = "III";
+                displayImage.sprite = rune3;
                 break;
             case 4:
                 displayText.text = "IV";
+                displayImage.sprite = rune4;
                 break;
             case 5:
                 displayText.text = "V";
+                displayImage.sprite = rune5;
                 break;
             case 6:
                 displayText.text = "VI";
+                displayImage.sprite = rune6;
                 break;
             default: break;
         }
